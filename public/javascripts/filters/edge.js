a494a54b0d60964ebe959ead80e9c8054d5bf284
vagnervjs <vagnervjs@gmail.com>
2013-03-15 00:55:58 -0300

    Change the edge.js file

--- public/javascripts/filters/edge.js
+++ public/javascripts/filters/edge.js
@@ -2,34 +2,19 @@ onmessage = function(e) {
   var data = e.data
     , quant = []
     , edge = [];
-  
-  for (var i = 0, n = data.length; i < n; i += 4) {
-    var r = data[i] & 0xff
-      , g = data[i+1] & 0xff
-      , b = data[i+2] & 0xff
-      , gray = (r + g + b) / 3;
-    
-    quant.push(gray & 0xc0);
-  }
-    
-  for (var y = 1; y < 240 - 1; ++y) {
-    for (var x = 1; x < 320 - 1; ++x) {
-      var i = y * 320 + x
-        , c
-        , around = (quant[i-320] + quant[i-1] + quant[i+1] + quant[i+320]) / 4;
 
-      if (around < quant[i]) {
-        c = 0;
-      } else {
-        c = 255;
-      }
-      
-      edge[i*4] = c;
-      edge[i*4+1] = c;
-      edge[i*4+2] = c;
-      edge[i*4+3] = 255;
-    }
+
+  var d = data;
+  for (var i=0; i<d.length; i+=4) {
+    var r = d[i];
+    var g = d[i+1];
+    var b = d[i+2];
+    // CIE luminance for the RGB
+    // The human eye is bad at seeing red and blue, so we de-emphasize them.
+    var v = 0.2126*r + 0.7152*g + 0.0722*b;
+    d[i] = d[i+1] = d[i+2] = v
   }
+
   
-  postMessage(edge);
+  postMessage(d);
 };
